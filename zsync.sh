29f899378356b3203da3ada4b1604161798623e9
kien kennedy <kiennd@gmail.com>
2014-12-23 16:38:37 +0700

    update zsync.sh

--- zsync.sh
+++ zsync.sh
@@ -2,22 +2,43 @@
 
 # set path environment
 PATH="/usr/bin:/bin:/usr/sbin:/sbin"
-HOME="/root/bin"
 
-log="$HOME/zfssync.log"
+log="/var/log/zsync.log"
+[ -f $log ] || touch $log
 
 # set source and destination
-sdir="storage"
-ddir="backup/storage"
-dadr="root@10.0.0.30"
+src="storage"
+dst="backup/storage"
+srv="root@10.0.0.30"
+
+# Main
+case "$1" in 
+	hourly)
+		echo "Init"
+		;;
+	daily)
+		echo "Sync"
+		;;
+	*)
+		echo "Usage: zsync.sh {hourly|daily}"
+		exit 2
+esac
 
 # snapshot name
-hour1=$(date +%m%d_%H)
-hour0=$(date +%m%d_%H --date="-1 hour")
+if [ "$1" = "hourly" ]; then
+	time1="%m%d_%H"
+	time0="%m%d_%H --date='-1 hour'"
+elif [ "$1" = "daily" ]; then
+	time1="%m%d"
+	time0="%m%d --date='-1 day'"
+fi
 
-snap1="storage@$hour1"
-snap0="storage@$hour0"
+snap1="$src@$time1"
+snap0="$src@$time0"
 
 # run it now
-zfs snapshot $snap1
-zfs send -i $snap0 $snap1 | ssh $dadr zfs recv -vdF $ddir
+echo "### $time1 ###"                                           >> $log
+echo "zfs snapshot $snap1"                                      >> $log
+echo "zfs send -i $snap0 $snap1 | ssh $srv zfs recv -v $dst"    >> $log
+zfs snapshot $snap1 
+zfs send -i $snap0 $snap1 | ssh $srv zfs recv -v $dst 
